5614b001c721af5b9494bc511e01bdfbdf0bf933
Azamat Alishev <aoalishev@gmail.com>
2021-01-02 09:50:31 -0500

    Deleting

--- docker-gamad-cron/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-# VolumeMount type was read only, GAM app was trying to adjust file name but was unsuccesful
-# Mounting secrets to /credentials than copying them to /opt/gam/src/ solved the issue
-cp /credentials/* /root/.gam/
-
-exec $@
